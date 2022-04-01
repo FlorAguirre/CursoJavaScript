61a66a18edbe263f9ec7b907dc5f663070a16160
FlorAguirre <floraguirre1598@gmail.com>
2022-04-01 19:27:27 -0400

    1 - Desafio Complementario

---
+++ 1 - Desafio Complementario/js/miarchivo.js
@@ -0,0 +1,17 @@
+/* FOR */
+
+/* let suma = parseInt(prompt("Ingrese número"));
+
+for(let i = 1; i<= 10; i++){
+    let resultado = suma + i;
+    alert(suma + "+" + i + "=" + resultado);
+} */
+
+/* WHILE */
+
+let edad = prompt("Ingrese edad");
+
+while(edad >= 18) {
+    alert("El usuario es mayor de edad, tiene " + edad + " años");
+    edad = prompt("Ingrese otra edad");
+}
